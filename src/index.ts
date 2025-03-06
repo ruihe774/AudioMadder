af9e41fdc9efb710813c20575a15bb038754ecdd
Misaki Kasumi <misakikasumi@outlook.com>
2025-03-06 11:25:12 +0800

    do not wrap App

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,4 @@ import { render } from "solid-js/web";
 import App from "./App.tsx";
 import "./index.css";
 
-const root = document.getElementById("root")!;
-
-render(() => <App />, root);
+render(App, document.getElementById("root")!);
