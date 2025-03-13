b4567a0fbcfd0c4e04f6bf7d02d9ccd778f64880
Misaki Kasumi <misakikasumi@outlook.com>
2025-03-13 23:46:50 +0800

    pwa

--- build/vite.app.config.ts
+++ build/vite.app.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 import tailwindcss from "@tailwindcss/vite";
-import esmShResolve from "./esm-sh-resolve";
+import { VitePWA as pwa } from "vite-plugin-pwa";
 import htmlHeadTrim from "./html-head-trim";
 
 export default defineConfig({
@@ -13,6 +13,46 @@ export default defineConfig({
         }),
         tailwindcss(),
         htmlHeadTrim(),
+        pwa({
+            manifest: {
+                name: "Audio Madder",
+                short_name: "Audio Madder",
+                icons: [
+                    {
+                        src: "/pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                    {
+                        src: "/pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                    {
+                        src: "/pwa-maskable-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                    {
+                        src: "/pwa-maskable-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                ],
+                start_url: "/",
+                display: "standalone",
+                background_color: "#171717",
+                theme_color: "#171717",
+                description: "Spectrum visualizer using Web Audio API",
+            },
+            workbox: {
+                globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+            },
+        }),
     ],
     build: {
         target: ["chrome104", "firefox78", "safari14.1"],
