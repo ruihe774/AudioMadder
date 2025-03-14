0ece3a5afc3a4105c42b34811f15a7e1d5dfba61
Misaki Kasumi <misakikasumi@outlook.com>
2025-03-14 15:40:31 +0800

    fix pwa update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
 /* @refresh reload */
+import { registerSW } from "virtual:pwa-register";
 import { render } from "solid-js/web";
 import App from "./App.tsx";
 import "./index.css";
 
 render(App, document.getElementById("root")!);
+
+registerSW({
+    onRegisteredSW(swUrl, r) {
+        if (r) {
+            setTimeout(() => {
+                if (r.installing || !navigator) return;
+
+                if ("connection" in navigator && !navigator.onLine) return;
+
+                void fetch(swUrl, {
+                    cache: "no-store",
+                    headers: {
+                        "cache": "no-store",
+                        "cache-control": "no-cache",
+                    },
+                }).then((resp) => {
+                    if (resp?.status == 200) return r.update();
+                });
+            }, 1000);
+        }
+    },
+});
